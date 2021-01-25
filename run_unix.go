202bb6ec1066001bdfd282a6b252c4b05708e312
Sergio Lopez <slp@sinrega.org>
2021-01-25 14:01:01 +0100

    On non-Linux systems return a valid DefaultNamespaceOptions
    On non-Linux systems, return a valid DefualtNamespaceOptions with all
    namespaces disabled, so we can at least create and mount images.
    
    Signed-off-by: Sergio Lopez <slp@redhat.com>

--- run_unix.go
+++ run_unix.go
@@ -4,6 +4,7 @@ package buildah
 
 import (
 	"github.com/opencontainers/runc/libcontainer/configs"
+	"github.com/opencontainers/runtime-spec/specs-go"
 	"github.com/pkg/errors"
 )
 
@@ -20,5 +21,14 @@ func (b *Builder) Run(command []string, options RunOptions) error {
 	return errors.New("function not supported on non-linux systems")
 }
 func DefaultNamespaceOptions() (NamespaceOptions, error) {
-	return NamespaceOptions{}, errors.New("function not supported on non-linux systems")
+	options := NamespaceOptions{
+		{Name: string(specs.CgroupNamespace), Host: false},
+		{Name: string(specs.IPCNamespace), Host: false},
+		{Name: string(specs.MountNamespace), Host: false},
+		{Name: string(specs.NetworkNamespace), Host: false},
+		{Name: string(specs.PIDNamespace), Host: false},
+		{Name: string(specs.UserNamespace), Host: false},
+		{Name: string(specs.UTSNamespace), Host: false},
+	}
+	return options, nil
 }
